83f42cded5de218f9b6ce982a47ef58f0ede91ed
peacedebbie120-bit <peacedebbie120@gmail.com>
2025-12-20 15:26:16 +0200

    Add files via upload

---
+++ main.java.js
@@ -0,0 +1,10 @@
+let images = [
+    https://en-tbn0.gstatic.com/images?q=tbn:ANd9GcR9YYh5Fk1u9VsWWr1MhkyQeOzeNbtnnMO96g&s,
+    https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZExXZfuy_mIpsM-T69cjwfWlNiY8MJK_shQ&s,
+    https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQVw1P6YdldCQB6vRrcrUm7qcmzNj7LM8oVg&s,
+    https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoBuMvSuYezLE9rwI-zOJeIOmcIGfDPqOvFA&s
+]
+let index
+
+
+document.getElementById("Slider").src = images[index - 1]
